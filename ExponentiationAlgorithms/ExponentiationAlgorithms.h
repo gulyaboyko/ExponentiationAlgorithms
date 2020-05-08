094deef77027ba1449bb8951a2f97a1f2a789730
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-05-08 13:57:56 +0300

    Initial Commit

---
+++ ExponentiationAlgorithms/ExponentiationAlgorithms.h
@@ -0,0 +1,19 @@
+//
+//  ExponentiationAlgorithms.h
+//  ExponentiationAlgorithms
+//
+//  Created by Gulya Boiko on 5/8/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExponentiationAlgorithms.
+FOUNDATION_EXPORT double ExponentiationAlgorithmsVersionNumber;
+
+//! Project version string for ExponentiationAlgorithms.
+FOUNDATION_EXPORT const unsigned char ExponentiationAlgorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExponentiationAlgorithms/PublicHeader.h>
+
+
